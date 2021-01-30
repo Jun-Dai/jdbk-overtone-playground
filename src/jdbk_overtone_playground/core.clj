acddf240ebb15bffc24b0d8b497e87655dfc40e7
Jun-Dai Bates-Kobashigawa <bako@taraval.net>
2021-01-30 16:09:15 +0000

    Just playin' around with two borrowed instruments.

--- src/jdbk_overtone_playground/core.clj
+++ src/jdbk_overtone_playground/core.clj
@@ -1,4 +1,51 @@
 (ns jdbk-overtone-playground.core (:gen-class))
 
-(defn -main [& args]
-  (println (str "Hello, " (if args (first args) "World") "!")))
+(use 'overtone.live)
+
+(require 'jdbk-overtone-playground.inst)
+(refer 'jdbk-overtone-playground.inst)
+
+(defn play-freq
+  [metronome beat instrument [freq dur]]
+  (let [end (+ beat dur)]
+    (if note
+      (let [id (at (metronome beat) (instrument freq))]
+        (at (metronome end) (ctl id :gate 0))))
+    end))
+
+(defn play-note
+  [metronome beat inst [note dur]]
+  (let [freq (if note (midicps note))]
+    (play-freq metronome beat inst [freq dur])))
+
+
+(defn play-ratio
+  [metronome beat instrument orig [ratio dur]]
+  (let [freq (if ratio (* orig ratio) nil)]
+    (play-freq metronome beat instrument [freq dur])))
+
+(defn one-twenty-bpm [] (metronome 120))
+(def otb one-twenty-bpm)
+
+
+(defn play-freq-score
+  ([inst score] (play-freq-score (one-twenty-bpm) inst score))
+  ([mnome inst score] (play-freq-score mnome (mnome) inst score))
+  ([mnome beat instrument score]
+   (let [cur-note (first score)]
+       (when cur-note
+         (let [next-beat (play-freq mnome beat instrument cur-note)]
+           (println "next-beat: " next-beat)
+           (apply-at (mnome next-beat) play-freq-score mnome next-beat instrument
+                     (next score) []))))))
+
+(defn test-plucked [& args]
+  (play-freq-score plucked-string [[440 1] [660 2] [550 1] [880 2]]))
+
+(defn test-organ [& args]
+  (play-freq-score organ [[440 1] [660 2] [550 1] [880 2]]))
+
+
+
+
+
---
+++ src/jdbk_overtone_playground/inst.clj
@@ -0,0 +1,26 @@
+(ns jdbk-overtone-playground.inst (:gen-class))
+
+(use 'overtone.live)
+
+;; snagged from https://github.com/overtone/overtone/blob/master/src/overtone/examples/getting_started/video.clj
+;; modified to take frequency instead of note
+(definst plucked-string [freq 440 amp 0.8 dur 2 decay 30 coef 0.3 gate 1]
+  (let [noize  (* 0.8 (white-noise))
+        dly    (/ 1.0 freq)
+        plk    (pluck noize gate dly dly decay coef)
+        dist   (distort plk)
+        filt   (rlpf dist (* 12 freq) 0.6)
+        clp    (clip2 filt 0.8)
+        reverb (free-verb clp 0.4 0.8 0.2)]
+    (* amp (env-gen (perc 0.0001 dur)) reverb)))
+
+
+;; snagged from https://github.com/ctford/dueling-keyboards/blob/master/src/dueling_keyboards/instrument.clj
+(definst organ [freq 440 dur 5.0 vol 0.3 wet 0.5 room 0.5 limit 5000 attack 0.1]
+  (->
+    (map #(sin-osc (* freq %)) (range 1 5))
+    mix
+    (lpf limit)
+    (free-verb :mix 0.5 :damp wet :room room)
+    (* (env-gen (asr attack 1.0 0.5) (line:kr 1.0 0.0 dur) :action FREE))
+    (* vol)))
