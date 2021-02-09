4c4dfb5bf090a657ebed8ed1b1831199b7d4b0f3
Jun-Dai Bates-Kobashigawa <bako@taraval.net>
2021-02-09 19:04:24 +0000

    Adding a library that makes it easier to refresh in lein repl

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jdbk-overtone-playground "0.0.1"
-  :dependencies [[overtone/overtone "0.10.6"]]
+  :dependencies [[overtone/overtone "0.10.6"] [org.clojure/tools.namespace "0.2.4"]]
   :native-path "native"
 ;  :source-paths ["src"]
   :main jdbk-overtone-playground.core)
