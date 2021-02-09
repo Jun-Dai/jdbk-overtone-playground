a7e560fd0dc4381d074c65465aab9fec2f02a433
Jun-Dai Bates-Kobashigawa <bako@taraval.net>
2021-02-09 19:04:56 +0000

    Adding an instrument based on a single-note piano sample.

--- src/jdbk_overtone_playground/core.clj
+++ src/jdbk_overtone_playground/core.clj
@@ -5,46 +5,64 @@
 (require 'jdbk-overtone-playground.inst)
 (refer 'jdbk-overtone-playground.inst)
 
+(defn halt [coll]
+  (map #(ctl % :gate 0) coll))
+
 (defn play-freq
   [metronome beat instrument [freq dur]]
   (let [end (+ beat dur)]
-    (if note
+    (if freq
       (let [id (at (metronome beat) (instrument freq))]
         (at (metronome end) (ctl id :gate 0))))
     end))
 
-(defn play-note
-  [metronome beat inst [note dur]]
-  (let [freq (if note (midicps note))]
-    (play-freq metronome beat inst [freq dur])))
-
-
-(defn play-ratio
-  [metronome beat instrument orig [ratio dur]]
-  (let [freq (if ratio (* orig ratio) nil)]
-    (play-freq metronome beat instrument [freq dur])))
-
 (defn one-twenty-bpm [] (metronome 120))
 (def otb one-twenty-bpm)
 
-
-(defn play-freq-score
-  ([inst score] (play-freq-score (one-twenty-bpm) inst score))
-  ([mnome inst score] (play-freq-score mnome (mnome) inst score))
+(defn play-freq-melody
+  ([inst score] (play-freq-melody (one-twenty-bpm) inst score))
+  ([mnome inst score] (play-freq-melody mnome (mnome) inst score))
   ([mnome beat instrument score]
    (let [cur-note (first score)]
-       (when cur-note
+      (when cur-note
          (let [next-beat (play-freq mnome beat instrument cur-note)]
-           (println "next-beat: " next-beat)
-           (apply-at (mnome next-beat) play-freq-score mnome next-beat instrument
+           (apply-at (mnome next-beat) play-freq-melody mnome next-beat instrument
                      (next score) []))))))
 
+(defn ratio->hz [orig [ratio dur]]
+  (if ratio [(* orig ratio) dur] [ratio dur]))
+
+(defn ratioscore->hz [orig score]
+  (map (partial ratio->hz orig) score))
+
+(defn midinote->hz [[note dur]]
+  (if note [(midi->hz note) dur] [note dur]))
+
+(defn midiscore->hz [score]
+  (map midinote->hz score))
+
+
+(def melody {:type :freq :notes [[440 2] [660 2] [528 2] [440 2]]})
+(def melody2 {:type :ratio :orig 330 :notes [[nil 2] [1/1 2] [3/2 2] [6/5 2] [1/1 2]]})
+(def melody3 {:type :midinotes :notes [[61 2] [68 2] [64 2] [61 2] [nil 2] [60 4]]})
+
+(defn play-score
+  ([score] (play-score (:inst score) (score)))
+  ([inst score]
+   (case (:type score)
+     :freq (play-freq-melody inst (:notes score))
+     :ratio (play-freq-melody inst (ratioscore->hz (:orig score) (:notes score)))
+     :midinotes (play-freq-melody inst (midiscore->hz (:notes score))))))
+
+(defn jdbk-demo []
+;  (play-score plucked-string melody3)
+  (play-score organ melody2))
+
 (defn test-plucked [& args]
-  (play-freq-score plucked-string [[440 1] [660 2] [550 1] [880 2]]))
+  (play-freq-melody plucked-string [[440 1] [660 2] [550 1] [880 2]]))
 
 (defn test-organ [& args]
-  (play-freq-score organ [[440 1] [660 2] [550 1] [880 2]]))
-
+  (play-freq-melody organ [[440 1] [660 2] [550 1] [880 2]]))
 
 
 
--- src/jdbk_overtone_playground/inst.clj
+++ src/jdbk_overtone_playground/inst.clj
@@ -16,11 +16,18 @@
 
 
 ;; snagged from https://github.com/ctford/dueling-keyboards/blob/master/src/dueling_keyboards/instrument.clj
-(definst organ [freq 440 dur 5.0 vol 0.3 wet 0.5 room 0.5 limit 5000 attack 0.1]
+(definst organ [freq 440 dur 5.0 vol 0.3 wet 0.5 room 0.5 limit 5000 attack 0.1 gate 1]
   (->
     (map #(sin-osc (* freq %)) (range 1 5))
     mix
     (lpf limit)
     (free-verb :mix 0.5 :damp wet :room room)
-    (* (env-gen (asr attack 1.0 0.5) (line:kr 1.0 0.0 dur) :action FREE))
+    (* (env-gen (asr attack 1.0 0.5) (line:kr 1.0 0.0 dur) :gate gate :action FREE))
     (* vol)))
+
+
+;; take a single piano A4 sample and adjust the sample speed to get different pitches
+(def simple-piano-sample (freesound-sample 374311))
+(definst simple-piano [freq 440 gate 1]
+  (let [env (env-gen (adsr 0 1 1 0.1) :gate gate :action FREE)]
+    (scaled-play-buf 2 simple-piano-sample :rate (/ freq 440))))
